b5c5e6098aead9481b61ef3d11c9e1dc567e35ec
moody <mm@supernines.com>
2024-07-14 13:43:45 +0100

    production version

---
+++ background.js
@@ -0,0 +1,89 @@
+let countdown;
+let endTime;
+let remainingTime = 0;
+let sessions = []; // Define the sessions array
+let breaks = []; // Define the breaks array
+let focusTimes = []; // Define the focus times array
+
+
+/// this pulls the data from the storage and sets it to the variables
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.type === "console") {
+      console.log(request.message);
+    }
+ });
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    if (message.minutes) {
+        chrome.action.setBadgeBackgroundColor({color: '#4688F1'});
+        chrome.action.setBadgeTextColor({color: '#FFFFFF'});
+        remainingTime = message.minutes * 60;
+        startCountdown(message.minutes); // Pass the minutes parameter
+        focusTimes.push(message.minutes); // Add the focus time to the array
+        chrome.storage.local.set({focusTimes: focusTimes}); // Save the focus times array to storage
+    } else if (message.pause) {
+        if (remainingTime > 0) {
+            clearInterval(countdown);
+        } 
+       // clearInterval(countdown);
+    } else if (message.resume) {
+        if (remainingTime > 0) {
+            startCountdown(remainingTime / 60); // Pass the remaining time in minutes
+        } 
+     // startCountdown(message.minutes); // Pass the minutes parameter
+    } else if (message.reset) {
+        clearInterval(countdown);
+        chrome.action.setBadgeText({text: ''});
+        remainingTime = 0;
+    } else if (message.break) {
+        breaks.push(message.minutes); // Add the break time to the array
+        chrome.storage.local.set({breaks: breaks}); // Save the breaks array to storage
+    } else if (message.request === 'remainingTime') {
+        sendResponse({remainingTime: remainingTime});
+    }
+    
+});
+
+
+function startCountdown(minutes) {
+    if (remainingTime <= 0) {
+        return;
+    }
+    endTime = Date.now() + remainingTime * 1000;
+    // Add the new session to the sessions array
+    sessions.push({start: Date.now(), duration: minutes * 60 * 1000});
+    // Save the sessions array to storage
+    chrome.storage.local.set({sessions: sessions});
+    countdown = setInterval(function() {
+        remainingTime = Math.round((endTime - Date.now()) / 1000);
+        if (remainingTime <= 0) {
+            clearInterval(countdown);
+      
+            chrome.action.setBadgeText({text: '0:00'});
+            chrome.action.setBadgeBackgroundColor({color: '#fb3b1e'});
+            chrome.action.setBadgeTextColor({color: '#FFFFFF'});
+
+            try {
+                chrome.runtime.sendMessage({command: "restartButton"});
+            } catch (error) {
+    
+                error = null;
+            }
+           //  chrome.runtime.sendMessage({command: "restartButton"});      
+       
+            // Save the new states to chrome.storage
+            chrome.storage.sync.set({startButton: 'none', pauseButton: 'none', resumeButton: 'none', resetButton: 'none', focusTimeSlider: 'none', restartButton: 'block'});
+            
+            // Update the end time and completed status of the last session
+            sessions[sessions.length - 1].end = Date.now();
+            sessions[sessions.length - 1].completed = true;
+            // Save the updated sessions array to storage
+            chrome.storage.local.set({sessions: sessions});
+            return
+        } else {
+        const minutes = Math.floor(remainingTime / 60);
+        const seconds = remainingTime % 60;
+        chrome.action.setBadgeText({text: `${minutes}${seconds < 10 ? '0' : ''}${seconds}`});
+        }
+    }, 1000);
+}
---
+++ popup.js
@@ -0,0 +1,227 @@
+let countdown;
+
+// Get references to the buttons and the slider
+let startButton = document.getElementById('startButton');
+let pauseButton = document.getElementById('pauseButton');
+let resumeButton = document.getElementById('resumeButton');
+let resetButton = document.getElementById('resetButton');
+let restartButton = document.getElementById('restartButton');
+let focusTimeSlider = document.getElementById('focusTimeSlider');
+
+
+function loadButtonStates() {
+  // Load the saved states from chrome.storage when the popup is opened
+    chrome.storage.sync.get(['startButton', 'pauseButton', 'resumeButton', 'resetButton', 'restartButton', 'focusTimeSlider'], function(result) {
+      startButton.style.display = result.startButton || 'block';
+      pauseButton.style.display = result.pauseButton || 'none';
+      resumeButton.style.display = result.resumeButton || 'none';
+      resetButton.style.display = result.resetButton || 'none';
+      restartButton.style.display = result.restartButton || 'none';
+      focusTimeSlider.style.display = result.focusTimeSlider || 'block';
+    });
+  }
+  // Call the function immediately to load the button states when the popup opens
+  loadButtonStates();
+
+// Get the current day
+let today = new Date().getDay();
+
+// Load the saved day from chrome.storage
+chrome.storage.sync.get(['savedDay'], function(result) {
+  // If the saved day is not today, reset the counters
+  if (result.savedDay !== today) {
+    // Reset the counters
+    focusTime.value = 30;
+    focusTimeValue.textContent = 0;
+
+    // Update the saved day in chrome.storage
+    chrome.storage.sync.set({savedDay: today});
+  }
+});
+
+
+// focus slider
+let focusTime = document.getElementById('focusTime');
+let focusTimeValue = document.getElementById('focusTimeValue');
+
+focusTime.oninput = function() {
+  focusTimeValue.textContent = this.value;
+}
+
+
+// Initially, only the start button should be visible
+pauseButton.style.display = 'none';
+resumeButton.style.display = 'none';
+resetButton.style.display = 'none';
+restartButton.style.display = 'none';
+
+// Buttion event listeners
+///
+document.getElementById('startButton').addEventListener('click', function() {
+    var minutes = parseFloat(document.getElementById('focusTime').value);
+    document.getElementById('startButton').style.display = 'none';
+    document.getElementById('resetButton').style.display = 'none';
+    document.getElementById('pauseButton').style.display = 'block';
+    document.getElementById('resumeButton').style.display = 'none';
+    document.getElementById('restartButton').style.display = 'none';
+    document.getElementById('focusTimeSlider').style.display = 'none';
+    // Save the new states to chrome.storage
+    chrome.storage.sync.set({startButton: 'none', pauseButton: 'block', resumeButton: 'none', resetButton: 'none', restartButton: 'none', focusTimeSlider: 'none'});
+    if (!isNaN(minutes)) {
+      chrome.runtime.sendMessage({minutes: minutes});
+    }
+  });
+
+  document.getElementById('pauseButton').addEventListener('click', function() {
+    chrome.runtime.sendMessage({pause: true});
+    document.getElementById('pauseButton').style.display = 'none';
+    document.getElementById('startButton').style.display = 'none';
+    document.getElementById('resetButton').style.display = 'block';
+    document.getElementById('resumeButton').style.display = 'block';
+    document.getElementById('restartButton').style.display = 'none';
+    // Save the new states to chrome.storage
+    chrome.storage.sync.set({startButton: 'none', pauseButton: 'none', resumeButton: 'block', resetButton: 'block', restartButton: 'none', focusTimeSlider: 'none'});
+  });
+
+  document.getElementById('resetButton').addEventListener('click', function() {
+    chrome.runtime.sendMessage({reset: true});
+    document.getElementById('resetButton').style.display = 'none';
+    document.getElementById('startButton').style.display = 'block';
+    document.getElementById('resumeButton').style.display = 'none';
+    document.getElementById('pauseButton').style.display = 'none';
+    document.getElementById('focusTimeSlider').style.display = 'block';
+    // Save the new states to chrome.storage
+    chrome.storage.sync.set({startButton: 'block', pauseButton: 'none', resumeButton: 'none', resetButton: 'none', focusTimeSlider: 'block', restartButton: 'block'});
+  });
+
+
+  document.getElementById('resumeButton').addEventListener('click', function() {
+    chrome.runtime.sendMessage({resume: true});
+    document.getElementById('resumeButton').style.display = 'none';
+    document.getElementById('pauseButton').style.display = 'block';
+    document.getElementById('resetButton').style.display = 'none';
+    document.getElementById('startButton').style.display = 'none';
+    document.getElementById('restartButton').style.display = 'none';
+    // Save the new states to chrome.storage
+    chrome.storage.sync.set({startButton: 'none', pauseButton: 'block', resumeButton: 'none', resetButton: 'none', focusTimeSlider: 'none', restartButton: 'none'});
+  });
+
+  document.getElementById('restartButton').addEventListener('click', function() {
+    chrome.runtime.sendMessage({restart: true});
+    document.getElementById('resumeButton').style.display = 'none';
+    document.getElementById('pauseButton').style.display = 'none';
+    document.getElementById('resetButton').style.display = 'none';
+    document.getElementById('startButton').style.display = 'block';
+    document.getElementById('restartButton').style.display = 'none';
+    document.getElementById('focusTimeSlider').style.display = 'block';
+    // Save the new states to chrome.storage
+    chrome.storage.sync.set({startButton: 'block', pauseButton: 'none', resumeButton: 'none', resetButton: 'none', focusTimeSlider: 'block', restartButton: 'none'});
+  });
+
+
+  function updateCountdown() {
+    chrome.runtime.sendMessage({request: 'remainingTime'}, function(response) {
+    let remainingTime = response.remainingTime;
+      //const remainingTime = response.remainingTime;
+      const minutes = Math.floor(remainingTime / 60);
+      const seconds = remainingTime % 60;
+
+      document.getElementById('countdown').textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+        // Log the countdown value
+       // console.log('Countdown:', countdown);
+    });
+  }
+  
+  function updateCount() {
+    chrome.storage.local.get('count', function(data) {
+//      document.getElementById('count').textContent = `Count: ${data.count || 0}`;
+    });
+  }
+  
+  chrome.storage.local.get('sessions', function(data) {
+    let sessions = data.sessions || [];
+    let sessionList = document.getElementById('sessionList');
+    sessionList.innerHTML = ''; // Clear the list
+
+    // Group sessions by day
+    let sessionsByDay = {};
+    for (let i = 0; i < sessions.length; i++) {
+        let session = sessions[i];
+        let day = new Date(session.start).toLocaleDateString();
+        if (!sessionsByDay[day]) {
+            sessionsByDay[day] = [];
+        }
+        sessionsByDay[day].push(session);
+    }
+
+    // Sort the days
+    let sortedDays = Object.keys(sessionsByDay).sort();
+
+    // Display the sessions
+    for (let i = 0; i < sortedDays.length; i++) {
+        let day = sortedDays[i];
+        let daySessions = sessionsByDay[day];
+        for (let j = 0; j < daySessions.length; j++) {
+            let session = daySessions[j];
+            let listItem = document.createElement('div');
+            let startTime = new Date(session.start).toLocaleTimeString();
+            let endTime = session.end ? new Date(session.end).toLocaleTimeString() : 'In progress';
+            let duration = session.duration / 1000 / 60; // Convert duration from ms to minutes
+          //  listItem.textContent = `Day: ${day}, Session ${j + 1}: Start time: ${startTime}, End time: ${endTime}, Duration: ${duration} minutes`;
+          
+            listItem.textContent = `Day: ${day}, Session ${j + 1}: Start time: ${startTime}, End time: ${endTime}, Duration: ${duration} minutes`;
+            sessionList.appendChild(listItem);
+        }
+    }
+});
+
+
+// Listen for commnds from background.js this 
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.command == "restartButton") {
+      let resetButton = document.querySelector('#restartButton');
+      resetButton.style.display = 'block';
+      restartButton.style.display = 'block';
+      pauseButton.style.display = 'none';
+      // Save the new states to chrome.storage
+      chrome.storage.sync.set({startButton: 'none', pauseButton: 'none', resumeButton: 'none', resetButton: 'none', focusTimeSlider: 'none', restartButton: 'block'});
+    }
+  });
+
+
+// Listen for changes in the storage area ans reset 
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+  console.log(changes);
+    // Log the changes to the console
+
+  // Check if the countdown has changed
+  if (changes.countdown) {
+    console.log('Countdown:', changes.countdown.newValue);
+    // Check if the countdown is zero
+    if (changes.countdown.newValue == 0) {
+
+      startButton.style.display = 'block';
+      pauseButton.style.display = 'block';
+      resumeButton.style.display = 'block';
+      resetButton.style.display = 'none';
+      focusTimeSlider.style.display = 'block';
+      
+      // Save the new states to chrome.storage
+      chrome.storage.sync.set({startButton: 'block', pauseButton: 'block', resumeButton: 'block', resetButton: 'block', focusTimeSlider: 'block', restartButton: 'block' });
+    }
+  }
+});
+
+// this is a way to send messages to the background.js file
+console.log = function(message) {
+  chrome.runtime.sendMessage({type: "console", message: message});
+};
+
+
+  setInterval(updateCountdown, 1000);
+  setInterval(updateCount, 1000);
+
+//window.addEventListener('unload', function() {
+ // clearInterval(countdown);
+///});
